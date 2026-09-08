5093b26a42fcd02c024f91b1a28b33a1cb52f0f6
rohambob212 <rohambob2244@gmail.com>
2026-09-08 15:43:04 +0330

    added database

--- memer/memer/spiders/telegram_spider.py
+++ memer/memer/spiders/telegram_spider.py
@@ -1,12 +1,16 @@
 import scrapy
 import re
 import yt_dlp as ydl
-class TimarestanSpider(scrapy.Spider):
-    name = 'timarestan'
+from pathlib import Path
+from diskcache import Cache
+class TelegramSpider(scrapy.Spider):
+    name = 'telegram'
     #https://t.me/troll_timarestan/20191?embed=1&mode=tme
     start_urls = []
+    chnl = "troll_timarestan"
+    data = Cache(Path("datas/" + chnl))
     for i in range(20000, 22000):
-        start_urls.append(f'https://t.me/troll_timarestan/{i}?embed=1&mode=tme')
+        start_urls.append(f'https://t.me/{chnl}/{i}?embed=1&mode=tme')
     # def start_requests(self):
     #     for url in self.start_urls:
     #         yield scrapy.Request(url, meta={"proxy": "socks5://127.0.0.1:1819"})
@@ -14,14 +18,14 @@ class TimarestanSpider(scrapy.Spider):
         vpost = response.css("video::attr(src)").get()
         if vpost:
             self.log(f"Parsing : {vpost}")
-            with ydl.YoutubeDL({"outtmpl": f"vids/{response.url.replace("https://t.me/troll_timarestan/","").replace("?embed=1&mode=tme","")}.%(ext)s"}) as dlr:
-                dlr.download(vpost)
+            # with ydl.YoutubeDL({"outtmpl": f"vids/{response.url.replace("https://t.me/troll_timarestan/","").replace("?embed=1&mode=tme","")}.%(ext)s"}) as dlr:
+            #     dlr.download(vpost)
         else:
             style = response.css(".tgme_widget_message_photo_wrap::attr(style)").get()
             impost = re.search(r"url\('(.+?)'\)", style).group(1) if style else None
             if impost:
                 self.log(f"Parsing : {impost}")
-                with ydl.YoutubeDL({"outtmpl": f"vids/{response.url.replace("https://t.me/troll_timarestan/","").replace("?embed=1&mode=tme","")}.%(ext)s"}) as dlr:
-                    dlr.download(impost)
+                # with ydl.YoutubeDL({"outtmpl": f"vids/{response.url.replace("https://t.me/troll_timarestan/","").replace("?embed=1&mode=tme","")}.%(ext)s"}) as dlr:
+                #     dlr.download(impost)
             else:
                 self.log(f"No media found : {response.url}")
